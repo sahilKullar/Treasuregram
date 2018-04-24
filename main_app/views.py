ae5602c400d6022631d4f34505df64dc24430c86
Sahil Kullar <sahil.kullar@capgemini.com>
2018-04-24 15:55:16 +0530

    css added.

--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,26 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse('<h1>Hello Explorers</h1>')
-    return render(request, 'index.html')
+    # name = 'Gold Nugget'
+    # value = 1000.00
+    # context = {
+    #     'treasure_name': name,
+    #     'treasure_val': value
+    # }
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html', {'treasures': treasures})
+
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA')
+]
