e6f04a03d22eb7ece3512ff56e9b5bd2944b35a3
Sahil Kullar <sahil.kullar@capgemini.com>
2018-04-24 12:40:43 +0530

    root added.

--- Treasuregram/urls.py
+++ Treasuregram/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    # url(r'^index/', views.index),
 ]
